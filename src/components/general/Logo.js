f65789f52f4e4f987e464a17a18ed38bbe7f8e92
mrcs22 <marcusmoraes2010@hotmail.com>
2021-06-29 13:51:32 -0300

    feat: user-sign-up

---
+++ src/components/general/Logo.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const LogoText = styled.h1`
+  font-family: "Saira Stencil One", cursive;
+  font-size: 38px;
+  letter-spacing: 1px;
+  color: #47597E;
+`;
+
+export default function Logo() {
+  return <LogoText>ToysCamp</LogoText>;
+}
