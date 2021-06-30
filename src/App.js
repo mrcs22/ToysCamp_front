68ac48e16e807cddd2f4f557d2e723ee9bd5a3d9
mrcs22 <marcusmoraes2010@hotmail.com>
2021-06-30 18:00:01 -0300

    feat: user sign in

--- src/App.js
+++ src/App.js
@@ -1,56 +1,67 @@
-import axios from "axios"
-import { useCallback, useEffect, useState } from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import HomePage from "./components/home/HomePage"
-import CategoryPage from "./components/categories/CategoryPage"
-import SignUpPage from "./components/signUp/SignUpPage"
-import GlobalStyle from "./styles/GlobalStyles"
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import CategoryPage from "./components/categories/CategoryPage";
+import SignUpPage from "./components/signUp/SignUpPage";
+import GlobalStyle from "./styles/GlobalStyles";
+import SignInPage from "./components/signIn/SignIn";
 
-export default function App(){
-    const [products, setProducts] = useState([])
+export default function App() {
+  const [products, setProducts] = useState([]);
+  const [user, setUser] = useState({});
 
-    const fetchProducts = useCallback(() => {
+  const fetchProducts = useCallback(() => {
+    axios
+      .get("http://localhost:4000/products")
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        alert("Ocorreu um erro. Tente novamente");
+      });
+  }, []);
 
-        axios.get("http://localhost:4000/products")
-        .then((res) => {
-            setProducts(res.data)
-        })
-        .catch((err) => {
-            alert("Ocorreu um erro. Tente novamente")
-        })
-    }, [])
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
 
-    useEffect(() => {
-        fetchProducts()
-    }, [fetchProducts])
-
-    return (
-        <BrowserRouter>
-            <GlobalStyle/>
-            <Switch>
-                <Route path="/sign-up" exact component={SignUpPage}/>
-                <Route 
-                    path="/" 
-                    exact 
-                    render={(props) => (
-                        <HomePage {...props} products={products} />
-                    )}
-                />
-                <Route 
-                    path="/releases" 
-                    exact 
-                    render={(props) => (
-                        <CategoryPage {...props} products={products} category={"Lançamentos"}/>
-                    )}
-                />
-                <Route 
-                    path="/sales" 
-                    exact 
-                    render={(props) => (
-                        <CategoryPage {...props} products={products} category={"Promoções"}/>
-                    )}
-                />
-            </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/sign-in" exact>
+          <SignInPage setUser={setUser} />
+        </Route>
+        <Route path="/sign-up" exact component={SignUpPage} />
+        <Route
+          path="/"
+          exact
+          render={(props) => <HomePage {...props} products={products} />}
+        />
+        <Route
+          path="/releases"
+          exact
+          render={(props) => (
+            <CategoryPage
+              {...props}
+              products={products}
+              category={"Lançamentos"}
+            />
+          )}
+        />
+        <Route
+          path="/sales"
+          exact
+          render={(props) => (
+            <CategoryPage
+              {...props}
+              products={products}
+              category={"Promoções"}
+            />
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 }
