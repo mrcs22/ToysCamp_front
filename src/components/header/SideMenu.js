909506aa564e4ded83ea29d570732c464ac438f6
Daniel Capeans <dcapeans@gmail.com>
2021-06-29 23:59:20 -0300

    fixed side menu animation

--- src/components/header/SideMenu.js
+++ src/components/header/SideMenu.js
@@ -2,21 +2,21 @@ import styled from "styled-components"
 
 export default function SideMenu({isOpen, toggleSideMenu}){
     return(
-        <Container isOpen={isOpen}>
-            <MenuContent>
-                <ul>
-                    <li>Início</li>
-                    <li>Lançamentos</li>
-                    <li>Promoções</li>
-                </ul>
-                <div className="sideMenuFooter">
-                    <div className="bar"></div>
-                    <span>Sair</span>
-                    <span>SAC: (99) 99999-9999</span>
-                </div>
-            </MenuContent>
-            <Background onClick={toggleSideMenu}/>
-        </Container>
+            <Container isOpen={isOpen}>
+                <MenuContent>
+                    <ul>
+                        <li>Início</li>
+                        <li>Lançamentos</li>
+                        <li>Promoções</li>
+                    </ul>
+                    <div className="sideMenuFooter">
+                        <div className="bar"></div>
+                        <span>Sair</span>
+                        <span>SAC: (99) 99999-9999</span>
+                    </div>
+                </MenuContent>
+                <Background onClick={toggleSideMenu} isOpen={isOpen}/>
+            </Container>
     )
 }
 
@@ -25,8 +25,8 @@ const Container = styled.div`
     left: ${props => props.isOpen ? "0" : "-100vw"};
     top: 0;
     /* transform: ${props => props.isOpen ? "translate3d(0vw, 0, 0)" : "translate3d(-100vw, 0, 0)"}; */
-    transition: left 1s ease;
-    display: ${props => props.isOpen ? "flex" : "none"};
+    transition: left 0.6s ease;
+    display: flex;
     position: fixed;
     width: 100%;
     height: 100%;
@@ -39,13 +39,16 @@ const MenuContent = styled.div`
     flex-direction: column;
     justify-content: space-between;
     width: 80%;
-    height: calc(100%);
+    height: 100%;
     padding: 0 20px;
     background-color: #fff;
+    z-index: 2;
+    font-family: "Saira Stencil One", cursive;
     ul {
-        margin-top: 90px;
+        margin-top: 120px;
     }
     ul li{
+        font-weight: 700;
         margin-bottom: 50px;
     }
     .sideMenuFooter{
@@ -53,7 +56,7 @@ const MenuContent = styled.div`
         flex-direction: column;
         justify-content: space-between;
         height: 100px;
-        font-size: 24px;
+        font-size: 20px;
         margin-bottom: 30px;
     }
     .bar {
@@ -65,6 +68,12 @@ const MenuContent = styled.div`
 `
 
 const Background = styled.div`
-    width: 20%;
-    background-color: rgba(0, 0, 0, 0.75);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: ${props => props.isOpen ? "0.6" : "0"};
+    transition: opacity 0.7s ease;
+    background-color: #000;
 `
